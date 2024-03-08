0eafe532555443e5a29c02b35bb603ed77348cc8
mrsankhl <mrityunjay.s.sankhla@oracle.com>
2024-03-08 13:33:36 +0530

    In previous commit, mistakenly implemented the story 4 with wrong intent With this commit, refactored the entire code to make functions follow single responsibility function current functionality: with flag -l, -w, -c used separately we can get the number of lines, words, and characters in a given file
    Also if none of the flag is passed along with a list of files, we get all 3 details for all the passed files
    if there are multiple files passed then after showing line, word and character counts for individual files, we show sum of all 3 details as total
    Also corrected the format of result to match it with wc results

--- wc.go
+++ wc.go
@@ -12,7 +12,7 @@ import(
 )
 
 // function to get the content of the file, or through relevant error
-func getFileContent(fileName string) ([]uint8, error) {
+func getFileContent(fileName string) (*[]uint8, error) {
 	/*
 	content, err := os.ReadFile(fileName)
 	if err != nil {
@@ -28,82 +28,67 @@ func getFileContent(fileName string) ([]uint8, error) {
 	if err != nil {
 		if os.IsNotExist(err) {
 			//fmt.Println("IsNotExist " + err.Error())
-			return content, errors.New(fmt.Sprintf("./wc: %s: open: No such file or directory\n", fileName))
+			return &content, errors.New(fmt.Sprintf("./wc: %s: open: No such file or directory\n", fileName))
 		} else if os.IsPermission(err) {
 			//fmt.Println("IsPermission " + err.Error())
-			return content, errors.New(fmt.Sprintf("./wc: %s: open: Permission denied\n", fileName))
+			return &content, errors.New(fmt.Sprintf("./wc: %s: open: Permission denied\n", fileName))
 		} else {
 			//fmt.Println("Error opening file " + err.Error())
-			return content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
+			return &content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
 		}
-		return content, err
+		return &content, err
 	}
 
 	// check if returned file is a directory or not
 	fileInfo, err := file.Stat()
 	if err!=nil {
 		//fmt.Println("Error fetching fileInfo: " + err.Error())		
-		return content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
+		return &content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
 	}
 	if fileInfo.Mode().IsDir() {
 		//fmt.Println(fileName + " is a directory ")
-		return content, errors.New(fmt.Sprintf("./wc: %s: read: Is a directory\n", fileName))
+		return &content, errors.New(fmt.Sprintf("./wc: %s: read: Is a directory\n", fileName))
 	}
 
 	// read the contents of the file
 	content, err = io.ReadAll(file)
 	if err!=nil {
-		return content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
+		return &content, errors.New(fmt.Sprintf("./wc: %s: %v\n", fileName, err.Error()))
 	}
 	//fmt.Println(content)
 
-	return content, nil
+	return &content, nil
 }
 
 
-func getLinesInFile(fileName string) (*[]string, error) {
+func getLinesInFile(fileContent *string) (*[]string) {
 	var lines []string
-	// check whether the give fileName is a directory or a file 
-	content, err := getFileContent(fileName)
-	if err != nil {
-		return &lines, err
-	}
 	// split the string with '\n' as delimiter
-	lines = strings.Split(string(content), "\n")
-	
-	/*
-	for _, line := range lines {
-		fmt.Println(line)
-	}
-	*/
+	lines = strings.Split(*fileContent, "\n")
 
 	// to avoid miscalculating the last empty line
 	if lines[len(lines)-1] == "" {
 		lines = lines[:len(lines)-1]
 	}
 
-	return &lines, nil
+	return &lines
 }
 
+
 // function to get the total words in the file
-func getWordsInFile(fileName string) (*[]string, error) {
+func getWordsFromLines(lines *[]string) (*[]string) {
 	var words []string
 	
-	lines, err := getLinesInFile(fileName)
-	if err != nil {
-		//fmt.Println(err.Error())
-		return &words, err	
-	}
-	//fmt.Printf("\t%d %v\n", len(*lines), *fileName)
-
 	// split each line with whitespace as delimiter and accumulate all the words
 	for _, line := range *lines {
 		wordsInLine := strings.Split(line, " ")
 		words = append(words, wordsInLine...) 
 	}
-	return &words, nil	
+
+	return &words	
 }
 
+/*
 // function to calculate total number of bytes in a file
 func getBytesInFile(fileName string) (int, error) {
 	var totalWords = 0
@@ -114,17 +99,16 @@ func getBytesInFile(fileName string) (int, error) {
 	totalWords = len(fileContent)
 	return totalWords, nil
 }
+*/
 
 // function to count total number of characters in a file
-func getTotalCharacters(fileName string) (int, error) {
+func getTotalCharacters(fileContent *string) (int) {
 	var totalCharacters = 0
-	fileContent, err := getFileContent(fileName)
-	if err != nil {
-		return totalCharacters, err
-	}
-	totalCharacters = len(string(fileContent))
-	return totalCharacters, nil
+	totalCharacters = len(string(*fileContent))
+	return totalCharacters 
 }
+
+// print results
 
 func main(){
 	/*
@@ -137,27 +121,35 @@ func main(){
 	fmt.Println("Welcome to Word count program!!") 
 	var fileToReadForLines = flag.String("l", "", "No of lines in the file, (if present)")
 	var fileToReadForWords = flag.String("w", "", "No of words in the file, (if present)")
-	var fileToReadForBytes = flag.String("c", "", "No of bytes in the file, (if present)")
-	var fileToReadForCharacters = flag.String("m", "", "No of characters in the file, (if present)")
+//	var fileToReadForBytes = flag.String("c", "", "No of bytes in the file, (if present)")
+	var fileToReadForCharacters = flag.String("c", "", "No of characters in the file, (if present)")
 
 	flag.Parse()
 
+	// fileContentInBytes : a pointer to file content in bytes, *[]uint8
+	// fileContentInBytes, err := getFileContent()
 	if *fileToReadForLines != "" {
-		lines, err := getLinesInFile(*fileToReadForLines)
+		fileContentInBytes, err := getFileContent(*fileToReadForLines)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(0)
 		}
-		fmt.Printf("\t%d %v\n", len(*lines), *fileToReadForLines)
+		fileContent := string(*fileContentInBytes)
+		lines := getLinesInFile(&fileContent)
+		fmt.Printf("%8d %s\n", len(*lines), *fileToReadForLines)
 	} 
 	if *fileToReadForWords != "" {
-		words, err := getWordsInFile(*fileToReadForWords)
+		fileContentInBytes, err := getFileContent(*fileToReadForWords)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(0)
 		}
-		fmt.Printf("\t%d %v\n", len(*words), *fileToReadForWords)
+		fileContent := string(*fileContentInBytes)
+		lines := getLinesInFile(&fileContent)
+		words := getWordsFromLines(lines)
+		fmt.Printf("%8d %s\n", len(*words), *fileToReadForWords)
 	} 
+	/*
 	if *fileToReadForBytes != "" {
 		totalBytes, err := getBytesInFile(*fileToReadForBytes)
 		if err != nil {
@@ -166,13 +158,65 @@ func main(){
 		}
 		fmt.Printf("\t%d %v\n", totalBytes, *fileToReadForBytes)
 	} 
+	*/
 
 	if *fileToReadForCharacters != "" {
-		totalChars, err := getTotalCharacters(*fileToReadForCharacters)
+		fileContentInBytes, err := getFileContent(*fileToReadForCharacters)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(0)
 		}
-		fmt.Printf("\t%d %v\n", totalChars, *fileToReadForCharacters)
+		fileContent := string(*fileContentInBytes)
+		totalChars := getTotalCharacters(&fileContent)
+		fmt.Printf("%8d %s\n", totalChars, *fileToReadForCharacters)
 	} 
+
+	// if none of the flags are used then print all three values total number of lines, words and bytes
+	if *fileToReadForWords == "" && *fileToReadForLines == "" && *fileToReadForCharacters == "" {
+		// if there is atleast one file passed as an argument
+		if len(os.Args) > 1 {
+			filesToRead := os.Args[1:]
+			totalLines, totalWords, totalChars := 0, 0, 0
+			for _, file := range filesToRead {
+				fileContentInBytes, err := getFileContent(file)
+				if err != nil {
+					fmt.Println(err.Error())
+					os.Exit(0)
+				}
+				fileContent := string(*fileContentInBytes)
+				chars := getTotalCharacters(&fileContent)
+				lines := getLinesInFile(&fileContent)
+				words := getWordsFromLines(lines)
+				fmt.Printf("%8d %8d %8d %s\n", len(*lines), len(*words), chars, file)
+				
+				// get the total number of lines, words and characters in all the files, if there are atleast 2 files
+				if len(filesToRead) > 1 {
+					totalLines += len(*lines)
+					totalWords += len(*words)
+					totalChars += chars
+				}
+					
+			}
+			if len(filesToRead) > 1 {
+				fmt.Printf("%8d %8d %8d %s", totalLines, totalWords, totalChars, "total") 
+			}
+		}
+	}   
 }
+
+/*
+// Testing Commands:
+
+./wc -l shakespeare-db/testTextNoRead.txt
+./wc -l shakespeare-db/testTextNoRead.txtadsfa
+./wc -l shakespeare-db/testText.txt
+./wc -l shakespeare-db/
+./wc -w shakespeare-db/testTextNoRead.txt
+./wc -w shakespeare-db/testTextNoRead.txtadsfa
+./wc -w shakespeare-db/testText.txt
+./wc -w shakespeare-db/
+./wc -c shakespeare-db/testTextNoRead.txt
+./wc -c shakespeare-db/testTextNoRead.txtadsfa
+./wc -c shakespeare-db/testText.txt
+./wc -c shakespeare-db/
+*/
